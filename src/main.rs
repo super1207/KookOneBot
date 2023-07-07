e3cd68fb75d3bbe62c8aa3b7dbf356feeef2b21d
super1207 <1875159423@qq.com>
2023-07-07 23:19:32 +0800

    适配某些无法获得utc_offset的环境

--- src/main.rs
+++ src/main.rs
@@ -64,13 +64,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     // 初始化日志
     let format = "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]";
+
+    // 获得utc偏移
+    let utc_offset;
+    if let Ok(v) = UtcOffset::current_local_offset() {
+        utc_offset = v;
+    } else {
+        // 中国是东八区，所以这里写8 hour
+        utc_offset = UtcOffset::from_hms(8,0,0).unwrap();
+    }
+
     tracing_subscriber::fmt()
     .with_timer(tracing_subscriber::fmt::time::OffsetTime::new(
-        UtcOffset::current_local_offset().unwrap(),
+        utc_offset,
         format_description::parse(format).unwrap(),
     )).with_max_level(tracing::Level::INFO)
     .init();
-
 
     log::warn!("欢迎使用KookOnebot by super1207!!! v0.0.7");
 
